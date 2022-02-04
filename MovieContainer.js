80e9d4276acbefdd5439fb448e4c37cd2c7420cd
Jasmyn Hicks <jasmynh44@gmail.com>
2022-02-03 20:21:07 -0700

    Add MovieContainer component and style it

--- MovieContainer.js
+++ MovieContainer.js
@@ -2,15 +2,22 @@ import React from 'react';
 import './MovieContainer.css'
 
 const MovieContainer = ({ movies }) => {
-    movies.map(movie => {
-        <Card
-            title={movie.title}
-            posterPath={movie.poster_path}
-            avgRating={movie.average_rating}
-            releaseDate={movie.release_date}
-            key={movie.id}
-        />
+    const movieCards = movies.map(movie => {
+        return (
+            <Card
+                title={movie.title}
+                posterPath={movie.poster_path}
+                avgRating={movie.average_rating}
+                releaseDate={movie.release_date}
+                key={movie.id}
+            />
+        )
     })
+    return (
+        <section className='movie-container'>
+            {movieCards}
+        </section>
+    )
 }
 
 export default MovieContainer
