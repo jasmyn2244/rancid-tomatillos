8d169f92e7e79270d9a49ccc08c663468dd3a3f9
Alexis Newby <alexis.newby@gmail.com>
2022-02-06 13:37:20 -0700

    Add conditional rendering and spacing to individual movie view

--- src/Components/MovieDetails/MovieDetails.js
+++ src/Components/MovieDetails/MovieDetails.js
@@ -2,30 +2,36 @@ import React from "react";
 import './MovieDetails.scss';
 
 const MovieDetails = (props) => {
+
+  // formatMoney = (moneyValue) => {
+  //   return moneyValue.toLocaleString('en-US', {
+  //     style: 'currency',
+  //     currency: 'USD',
+  //   })
+  // }
+
   return (
     <section className='movie-details-container'>
       <section className='gen-movie'>
         <div>
-          <img className='movie-backdrop' src={props.selectedMovie.backdrop_path} alt={props.selectedMovie.title} />
+          <img src={props.selectedMovie.backdrop_path} alt={props.selectedMovie.title} />
         </div>
-        <div>
+        <div className='title-other-details'>
           <h2 className='movie-title'>{props.selectedMovie.title}</h2>
+          {props.selectedMovie.tagline && <h3>{props.selectedMovie.tagline}</h3>}
           <div className='row-movie-details'>
-            <p>Rating {props.selectedMovie.average_rating.toFixed(1)}</p>
+            <p>Rating: {props.selectedMovie.average_rating.toFixed(1)}</p>
             <p>Release Date: {props.selectedMovie.release_date}</p>
-            <p>Duration: 1 hour 20 minutes</p>
+            <p>Duration: {props.selectedMovie.runtime} minutes</p>
           </div>
           <button className='back-to-main-button' onClick={() => props.displayMain()}>Back to Main</button>
-          <p>This movie is about blah blah blah.</p>
+          <p className='overview'>{props.selectedMovie.overview}</p>
         </div>
       </section>
       <section className='movie-details'>
-        <section>
-          <p>Tagline</p>
-          <p>Genres</p>
-          <p>Revenue</p>
-          <p>Budge</p>
-        </section>
+        {props.selectedMovie.budget !== 0 ? <p>Budget: ${props.selectedMovie.budget}</p> : <p></p>}
+        {props.selectedMovie.revenue !== 0 ? <p>Revenue: ${props.selectedMovie.revenue}</p> : <p></p>}
+        <p>Genres: {props.selectedMovie.genres.join(', ')}</p>
       </section>
     </section>
   )
