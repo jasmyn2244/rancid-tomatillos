7d6bbd8bf51b6635c18283009ba9467652be08b6
Alexis Newby <alexis.newby@gmail.com>
2022-02-06 12:27:27 -0700

    More styling to individual movie view

--- src/Components/MovieDetails/MovieDetails.js
+++ src/Components/MovieDetails/MovieDetails.js
@@ -5,21 +5,21 @@ const MovieDetails = (props) => {
   return (
     <section className='movie-details-container'>
       <section className='gen-movie'>
-        <button className='back-to-main-button' onClick={() => props.displayMain()}>Back to Main</button>
-        <img className='movie-backdrop' src={props.selectedMovie.backdrop_path} alt={props.selectedMovie.title} />
-        <h2 className='movie-title'>{props.selectedMovie.title}</h2>
-      </section>
-      <section className='movie-details'>
-        <section>
+        <div>
+          <img className='movie-backdrop' src={props.selectedMovie.backdrop_path} alt={props.selectedMovie.title} />
+        </div>
+        <div>
+          <h2 className='movie-title'>{props.selectedMovie.title}</h2>
           <div className='row-movie-details'>
             <p>Rating {props.selectedMovie.average_rating.toFixed(1)}</p>
             <p>Release Date: {props.selectedMovie.release_date}</p>
             <p>Duration: 1 hour 20 minutes</p>
           </div>
-          <div>
-            <p>This movie is about blah blah blah.</p>
-          </div>
-        </section>
+          <button className='back-to-main-button' onClick={() => props.displayMain()}>Back to Main</button>
+          <p>This movie is about blah blah blah.</p>
+        </div>
+      </section>
+      <section className='movie-details'>
         <section>
           <p>Tagline</p>
           <p>Genres</p>
