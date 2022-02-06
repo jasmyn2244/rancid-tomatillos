271f55f4fb09e38dcfefceb889834e652559510e
Jasmyn Hicks <jasmynh44@gmail.com>
2022-02-06 10:34:36 -0700

    Add styling to homepage

--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -3,7 +3,7 @@ import './Cards.scss';
 
 const Cards = ({ title, posterPath, releaseDate, avgRating, id, chooseMovie }) => {
     return (
-        <section className='movie-cards' id={id} onClick={() => chooseMovie(id)}>
+        <section tabIndex='0' className='movie-cards' id={id} onClick={() => chooseMovie(id)}>
             <img src={posterPath} alt={`${title} movie poster`} />
             <p>{title}</p>
             <p>{avgRating.toFixed(1)}</p>
