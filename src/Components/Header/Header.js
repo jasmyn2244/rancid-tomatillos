43f0362cb4df1128ea5c8a33f08ed0e50b5f992f
Alexis Newby <alexis.newby@gmail.com>
2022-02-07 18:50:57 -0700

    Finish off styling for now

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './Header.scss';
+import logo from './Rancid.png'
 
 const Header = () => {
     return (
         <header>
+            {/* <img className='title' alt='logo' src={logo}/> */}
             <h1 className='title'>Rancid Tomatillos</h1>
         </header>
     )
