c1b23ac4deaf8ce24226d200b6008d9d96b9d497
Alexis Newby <alexis.newby@gmail.com>
2022-02-10 17:36:29 -0700

    Change App to functional component, change MovieContainer to class component

--- src/App.js
+++ src/App.js
@@ -5,57 +5,72 @@ import Header from './Components/Header/Header.js';
 import MovieContainer from './Components/MovieContainer/MovieContainer';
 import MovieDetails from './Components/MovieDetails/MovieDetails';
 import ErrorPage from './Components/ErrorPage/ErrorPage';
-import { getAllMovies, getSingleMovie } from './api-calls';
+// import { getAllMovies, getSingleMovie } from './api-calls';
 import { Route, Switch } from 'react-router-dom';
 
 
-class App extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      movies: [],
-      selectedMovie: {},
-      error: '',
-    };
-  }
 
-  componentDidMount = () => {
-    getAllMovies()
-      .then(cleanedData => this.setState({ movies: cleanedData }))
-      .catch(error => this.setState({ error: error }))
-  }
+const App = () => {
 
-  chooseMovie = (id) => {
-    getSingleMovie(id)
-      .then(cleanedData => this.setState({ selectedMovie: cleanedData }))
-  }
-
-  render() {
-    return (
-      <>
-        <Header />
-          {this.state.error && <h2>Sorry you got an error. Please try again.</h2>}
-        <Switch>
-          {/* Console Logs are showing up twice in clicking on a movie 
-          If it wasn't getting logged twice, we would only get the error page.
-          May need an async function to make sure we're seeing a loader while API calls is loading.
-          */}
-          <Route exact path="/:movie" render={ ( { match } ) => {
-            const { movie } = match.params;
-            if (movie !== this.state.selectedMovie.title) {
-              return <ErrorPage />
-            } else {
-              return <MovieDetails selectedMovie={this.state.selectedMovie} /> 
-            }
-          }
-        }
-        />
-          <Route exact path="/" component= { () => <MovieContainer movies={this.state.movies} chooseMovie={this.chooseMovie} /> } />
-          <Route path="*"> <ErrorPage /> </Route>
-        </Switch>
-      </>
-    )
-  }
+  return (
+    <>
+      <Header />
+      <Switch>
+        <Route exact path="/:movie"> <MovieDetails/> </Route>
+        <Route exact path="/"> <MovieContainer/> </Route>
+        <Route path="*"> <ErrorPage /> </Route>
+      </Switch>
+    </>
+  )
 }
+
+// class App extends React.Component {
+//   constructor() {
+//     super()
+//     this.state = {
+//       movies: [],
+//       selectedMovie: {},
+//       error: '',
+//     };
+//   }
+
+//   componentDidMount = () => {
+//     getAllMovies()
+//       .then(cleanedData => this.setState({ movies: cleanedData }))
+//       .catch(error => this.setState({ error: error }))
+//   }
+
+//   chooseMovie = (id) => {
+//     getSingleMovie(id)
+//       .then(cleanedData => this.setState({ selectedMovie: cleanedData }))
+//   }
+
+//   render() {
+//     return (
+//       <>
+//         <Header />
+//           {this.state.error && <h2>Sorry you got an error. Please try again.</h2>}
+//         <Switch>
+//           {/* Console Logs are showing up twice in clicking on a movie 
+//           If it wasn't getting logged twice, we would only get the error page.
+//           May need an async function to make sure we're seeing a loader while API calls is loading.
+//           */}
+//           <Route exact path="/:movie" render={ ( { match } ) => {
+//             const { movie } = match.params;
+//             if (movie !== this.state.selectedMovie.title) {
+//               return <ErrorPage />
+//             } else {
+//               return <MovieDetails selectedMovie={this.state.selectedMovie} /> 
+//             }
+//           }
+//         }
+//         />
+//           <Route exact path="/" component= { () => <MovieContainer movies={this.state.movies} chooseMovie={this.chooseMovie} /> } />
+//           <Route path="*"> <ErrorPage /> </Route>
+//         </Switch>
+//       </>
+//     )
+//   }
+// }
 
 export default App;
--- src/Components/MovieContainer/MovieContainer.js
+++ src/Components/MovieContainer/MovieContainer.js
@@ -2,9 +2,26 @@ import React from 'react';
 import './MovieContainer.scss'
 import Cards from '../Cards/Cards'
 import { Link } from 'react-router-dom';
+import { getAllMovies } from '../../api-calls.js'
 
-const MovieContainer = ({ movies, chooseMovie }) => {
-    const movieCards = movies.map(movie => {
+
+class MovieContainer extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            movies: [],
+            error: ''
+        }
+    }
+
+    componentDidMount = () => {
+        getAllMovies()
+            .then(cleanedData => this.setState({ movies: cleanedData }))
+            .catch(error => this.setState({ error: error }))
+    }
+
+    getMovieCards = () => {
+        const movieCards = this.state.movies.map(movie => {
         return (
             <Link to={`/${movie.title}`} key={movie.id}>
                 <Cards
@@ -12,16 +29,44 @@ const MovieContainer = ({ movies, chooseMovie }) => {
                     posterPath={movie.poster_path}
                     avgRating={movie.average_rating}
                     id={movie.id}
-                    chooseMovie={chooseMovie}
+                    // chooseMovie={chooseMovie}
                 />
             </Link>
+        )})
+
+        return movieCards
+    }
+
+    render() {
+        return (
+            <section className='movie-container' >
+                { this.getMovieCards() }
+            </section>
         )
-    })
-    return (
-        <section className='movie-container'>
-            {movieCards}
-        </section>
-    )
+    }
+
 }
 
 export default MovieContainer
+
+// const MovieContainer = ({ movies, chooseMovie }) => {
+//     const movieCards = movies.map(movie => {
+//         return (
+//             <Link to={`/${movie.title}`} key={movie.id}>
+//                 <Cards
+//                     title={movie.title}
+//                     posterPath={movie.poster_path}
+//                     avgRating={movie.average_rating}
+//                     id={movie.id}
+//                     chooseMovie={chooseMovie}
+//                 />
+//             </Link>
+//         )
+//     })
+    // return (
+    //     <section className='movie-container'>
+    //         {movieCards}
+    //     </section>
+    // )
+
+
